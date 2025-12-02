28492f0f87e817cde3a4fe3f0552fa907822c221
Maikel Eckelboom <maikeleckelboom@outlook.com>
2025-12-02 02:37:41 +0100

    test(introspect): remove obsolete error manifest snapshot test

--- packages/introspect/scripts/export-errors.ts
+++
@@ -1,9 +0,0 @@
-import { writeFileSync } from "node:fs";
-
-import { buildErrorRegistrySchema } from "../src/errors/export-json";
-
-const schema = buildErrorRegistrySchema();
-writeFileSync(
-  "dist/error-registry.schema.json",
-  JSON.stringify(schema, null, 2),
-);
